6308bce5a8ec607587f2c300c1ef5a3aafc16e89
EzraJ <36978811+EzraJ@users.noreply.github.com>
2026-03-13 17:34:54 -0800

    Fixed vector2 data type

--- include/lima/Vector2.hpp
+++ include/lima/Vector2.hpp
@@ -5,12 +5,12 @@
 namespace lima{
     class Vector2{
         public:
-            Vector2(uint64_t xIn = 0, uint64_t yIn = 0){
+            Vector2(int64_t xIn = 0, int64_t yIn = 0){
                 x = xIn; y = yIn;
             }
             ~Vector2(){
                 
             }
-            uint64_t x, y;
+            int64_t x, y;
     };
 }
