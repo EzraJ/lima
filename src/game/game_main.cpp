5b0565a763464f801cdd7a197507c312db649851
EzraJ <36978811+EzraJ@users.noreply.github.com>
2026-03-13 17:49:31 -0800

    This shouldn't be a ptr

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -9,14 +9,12 @@ int game_main(int argc, char** argv, lima::render* currentRender, lima::keyboard
 		float y = ((info.normY + 1)/2) * 255;
 		info.beanPtr->setBG((unsigned char) x, (unsigned char) y, (sin(info.time)+1.0)/2.0 * 255)
 	});
-
-	lima::TextInput* myInp = new lima::TextInput(currentRender, currentKeyboard, 10, 10, 5, 5);
-	myInp->SetActive(true);
-	while(myInp->EnteredText() != "quit"){
+	
+	lima::TextInput myInp(currentRender, currentKeyboard, 10, 10, 5, 5);
+	myInp.SetActive(true);
+	while(myInp.EnteredText() != "quit"){
 
 	}
-
-	delete myInp;
 
     return 1;
 }
